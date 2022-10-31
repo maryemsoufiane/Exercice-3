4506a0b4e96b6b1c96032020b656958e2dda9149
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-10-31 21:17:45 +0100

    tst

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿
+
 using System;
 
 namespace MyApplication
 {
     class Program
     {
-
-        static void initialiseTab(int[] tab)    // method
+        static void initialiseTab(int[] tab)
         {
             tab = new int[4];
             Console.WriteLine(" saisir les elements du tableau");
@@ -17,29 +17,102 @@ namespace MyApplication
 
         }
 
-        static void SortTab(int[] tab)   
+        static void SortTab(int[] tab)
         {
-        
-        for (int i = 0; i < tab.Length; i++)
-        {
-            for (int j = i; i < tab.Length; i++)
+            int k;
+            for (int i = 0; i < tab.Length; i++)
             {
-
-                if (tab[i] < tab[j])
+                for (int j = i; i < tab.Length; i++)
                 {
+
+                    if (tab[i] < tab[j])
+                    {
+                        k = tab[i];
+                        tab[i] = tab[j];
+                        tab[j] = k;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+
+
+
                 }
-
-
 
             }
 
         }
+
+
+
+
+        static void AfficheTab(int[] tab)
+        {
+        
+            
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                
+                    Console.WriteLine(tab[i]);
+            }
+
+        }
+
+
+        static void InsirerElem(int[] tab)
+        {
+            int cpt = 0;
+            int X;
+            Console.WriteLine(" saisir  L'elements que vous voullez insirer dans le tableau");
+            X = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] <X)
+                    {
+                       
+                    }
+                 else
+                    {
+                        continue;
+                    }
+
+
+
+
+                
+
+            }
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            int [] T= new int [4];
+            initialiseTab(T);
+            SortTab(T);
+            AfficheTab(T);
+
+
+        }
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
